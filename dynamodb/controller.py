33fbc2b8dde99506f13d0f50c01b66b56b22d49b
abunuwas <joseharoperalta@gmail.com>
2016-04-21 16:50:41 +0100

    Change name of controller module

--- dynamodb/controller.py
+++ dynamodb/controller.py
@@ -1,8 +1,10 @@
 
 
-class CameraController:
+class Controller:
 	def __init__(self, connectionManager):
 		self.cm = connectionManager
 	
+	def checkIfTableIsActive(self, table):
+		return True
 
 
