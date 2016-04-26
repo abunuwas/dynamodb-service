42854a299a807f1fedd3260a1570573f05305618
abunuwas <joseharoperalta@gmail.com>
2016-04-26 18:42:14 +0100

    Add tests for models logic

---
+++ models.py
@@ -0,0 +1,17 @@
+from models_package.models import Model, Key, Throughput
+
+class Camera(Model):
+	table_name = 'Camera'
+	year = Key(name='year', key_type='hash', attr_type='N')
+	title = Key(key_type='range', attr_type='N')
+	provisioned_throughput = Throughput(read=10, write=10)
+
+
+if __name__ == '__main__':
+	camera = Camera()
+	title = camera.title.get_values()
+	print(title)
+	print(camera.__dict__)
+	print(vars(camera))
+	year = getattr(camera, 'year')
+	print(camera.get_attributes())
