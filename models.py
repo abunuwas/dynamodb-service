449689bc00ec4fec8d518dd18c733427e461ecdb
abunuwas <joseharoperalta@gmail.com>
2016-04-27 18:08:55 +0100

    Further develop tests for the models

--- models.py
+++ models.py
@@ -15,3 +15,12 @@ if __name__ == '__main__':
 	print(vars(camera))
 	year = getattr(camera, 'year')
 	print(camera.get_attributes())
+	print(camera.get_table_name())
+	print(camera.get_required_items())
+
+	class Dog:
+		def __init__(*args):
+			print(args)
+
+	dog = Dog('height'=3, 'name'='Othelo')
+	print(dog)
